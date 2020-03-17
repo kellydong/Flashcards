54de908c9b8eebd827437ebe051bd9a5b15ce73e
Kelly Dong <dongweiling27@gmail.com>
2020-03-17 00:09:05 -0400

    lab 3 basics complete

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -8,15 +8,28 @@
 
 import UIKit
 
+struct Flashcard {
+    var question: String
+    var answer: String
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var backLabel: UILabel!
     @IBOutlet weak var frontLabel: UILabel!
+    
     @IBOutlet weak var card: UIView!
     
     @IBOutlet weak var answerA: UIButton!
     @IBOutlet weak var answerB: UIButton!
     @IBOutlet weak var answerC: UIButton!
+    
+    @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    var correctChoice = ""
+    var flashcards = [Flashcard]()
+    var currentIndex = 0
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     // Get the new view controller using segue.destination.
@@ -36,6 +49,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Read saved flashcards
+        readSavedFlashcards()
+        
+        if flashcards.count == 0 {
+            updateFlashcard(question: "What is the captial of Brazil?", answer: "Brasilia", choice1: "Rio de Janeiro", choice2: "Brasilia", choice3: "Sao Paulo")
+        }
+        else {
+            updateLabels()
+            updateNextPrevButtons()
+        }
             
         backLabel.layer.cornerRadius = 20.0
         frontLabel.layer.cornerRadius = 20.0
@@ -55,8 +79,8 @@ class ViewController: UIViewController {
         answerC.layer.borderWidth = 3.0
         answerC.layer.borderColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
         answerC.layer.cornerRadius = 20.0
-
     }
+    
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         if(frontLabel.isHidden == true){
             frontLabel.isHidden = false
@@ -67,23 +91,109 @@ class ViewController: UIViewController {
     }
     
     func updateFlashcard(question: String, answer: String, choice1: String, choice2: String, choice3: String){
+        let flashcard = Flashcard(question: question, answer: answer)
         self.frontLabel.text = question
         self.backLabel.text = answer
         self.answerA.setTitle(choice1, for: .normal)
         self.answerB.setTitle(choice2, for: .normal)
         self.answerC.setTitle(choice3, for: .normal)
+        
+        // making sure everything shows normally after changing the inputs
+        answerA.isHidden = false
+        answerB.isHidden = false
+        answerC.isHidden = false
+        frontLabel.isHidden = false
+        
+        correctChoice = answer
+        
+        // Adding flashcard in the flashcards array
+        flashcards.append(flashcard)
+        print("😎 Added new flashcard")
+        print("😎 We have now \(flashcards.count) flashcards")
+        currentIndex = flashcards.count - 1
+        print("😎 Our current index is \(currentIndex)")
+        updateNextPrevButtons()
+        updateLabels()
+        saveAllFlashcardsToDisk()
     }
     
     @IBAction func didTapA(_ sender: Any) {
-        answerA.isHidden = true
+        if(answerA.titleLabel?.text == correctChoice){
+            frontLabel.isHidden = true
+        }
+        else {
+            answerA.isHidden = true
+        }
     }
     
     @IBAction func didTapB(_ sender: Any) {
-        frontLabel.isHidden = true
+        if(answerB.titleLabel?.text == correctChoice){
+            frontLabel.isHidden = true
+        }
+        else {
+            answerB.isHidden = true
+        }
     }
     
     @IBAction func didTapC(_ sender: Any) {
-        answerC.isHidden = true
+        if(answerC.titleLabel?.text == correctChoice){
+            frontLabel.isHidden = true
+        }
+        else {
+            answerC.isHidden = true
+        }
+    }
+    
+    @IBAction func didTapOnPrev(_ sender: Any) {
+        currentIndex = currentIndex - 1
+        updateLabels()
+        updateNextPrevButtons()
+    }
+    @IBAction func didTapOnNext(_ sender: Any) {
+        currentIndex = currentIndex + 1
+        updateLabels()
+        updateNextPrevButtons()
+    }
+    
+    func updateNextPrevButtons() {
+        if currentIndex == flashcards.count - 1 {
+            nextButton.isEnabled = false
+        }
+        else {
+            nextButton.isEnabled = true
+        }
+        
+        if currentIndex == 0 {
+            prevButton.isEnabled = false
+        }
+        else {
+            prevButton.isEnabled = true
+        }
+    }
+    
+    func updateLabels() {
+        let currentFlashcard = flashcards[currentIndex]
+        
+        frontLabel.text = currentFlashcard.question
+        backLabel.text = currentFlashcard.answer
+        //answerA.setTitle(currentFlashcard.answerA, for: .normal)
+    }
+    
+    func saveAllFlashcardsToDisk() {
+        let dictionaryArray = flashcards.map { (card) -> [String: String] in return ["question": card.question, "answer": card.answer]
+        }
+        
+        UserDefaults.standard.set(dictionaryArray, forKey: "flashcards")
+        
+        print("Flashcards saved to UserDefaults")
+    }
+    
+    func readSavedFlashcards() {
+        if let dictionaryArray = UserDefaults.standard.array(forKey: "flashcards") as? [[String: String]] {
+            let savedCards = dictionaryArray.map { dictionary -> Flashcard in return Flashcard(question: dictionary["question"]!, answer: dictionary["answer"]!)
+            }
+            flashcards.append(contentsOf: savedCards)
+        }
     }
 }
 
--- Readme.md
+++ Readme.md
@@ -1,20 +1,18 @@
 ## Flashcards
-## Lab 2
+## Lab 3
 
 ### App Description
-This App is a simple flashcard app with one question. When the correct answer is picked, the answer will appear on top of the question. When a wrong answer is picked, the answer choice will be removed to hint the user that they chose the wrong answer. In addition to the default question, the user would also be able to edit the question, answer and multiple choice answers. If the user input is not valid, the program will created an error message. The user can back out of editing the questions and answer when clicking  the cancel button at the top. 
+This App is a simple flashcard app with one question. When the correct answer is picked, the answer will appear on top of the question. When a wrong answer is picked, the answer choice will be removed to hint the user that they chose the wrong answer. In addition to the default question, the user would also be able to edit the question, answer and multiple choice answers. If the user input is not valid, the program will created an error message. The user can back out of editing the questions and answer when clicking the cancel button at the top. After making a new flashcard, the card will be saved.
 
 ### App Walk-though
 
-<img src="http://g.recordit.co/mv9hoQAJZs.gif" width=200><br>
-
+<img src="http://g.recordit.co/GSvfzIHNwF.gif" width=200><br>
 
 ## Required
-- [x] User can open the creation screen
-- [x] User can cancel out of the creation screen
-- [x] User can enter a new question and answer in the creation screen to then show it on the flashcard
+- [x] User can browse through multiple flashcards
+- [x] User can re-open the app and see previously created flashcards
 - [x] Push code to GitHub
 ## Optional
-- [x] User gets an error if they try to create a new flashcard with no question or answer
-- [x] User can edit existing flashcard
-- [x] User can add multiple choice answers in the creation screen
+- [ ] User can delete a flashcard
+- [ ] User can edit existing flashcard
+- [ ] User can store multiple choice questions
